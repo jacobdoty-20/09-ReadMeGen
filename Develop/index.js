15903f05dc0dc8be99b413b305602a558c8d0f83
Snow Day <jakedoty@gmail.com>
2021-01-26 19:59:40 -0600

    added start code from class

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,60 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = [];
+const inquirer = require('inquirer');
+const fs = require('fs');
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is your user name?',
+      name: 'username',
+    },
+    {
+      type: 'password',
+      message: 'What is your password?',
+      name: 'password',
+    },
+    {
+      type: 'password',
+      message: 'Re-enter password to confirm:',
+      name: 'confirm',
+    },
+  ])
+  .then(response => 
+    // console.log(response)
+    // response.confirm === response.password
+    //   ? console.log('Success!')
+    //   : console.log('You forgot your password already?!')
+    {
+      let html = `<!DOCTYPE html>
+      <html lang="en">
+      <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Document</title>
+      </head>
+      <body>
+          ${response.username}
+      </body>
+      </html>`;
+      fs.writeFile("index.html", html, (err) => {
+        if(err){
+          console.log(err);
+        }
+        else{
+          console.log("created file");
+        }
+      })
+    }
+  );
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {}
